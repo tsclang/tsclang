eca7fae28686121b3883e9487e6fe6343c2aa10e
fwmakc <fwmakcpro@yandex.ru>
2026-06-10 15:06:24 +0300

    test: pool on desktop (10 new tests, 2 known-failing for gap analysis)

---
+++ test/cases/phase11/pool/embedded-pool-regression/expected.c
@@ -0,0 +1,32 @@
+#include "runtime.h"
+
+typedef struct { int32_t x; } Particle;
+typedef struct { bool has_value; Particle *value; int _pool_idx; } opt_ref_Particle;
+
+static Particle _particle_pool[4];
+static uint8_t _particle_pool_mask = 0;
+
+static opt_ref_Particle Particle_alloc(void) {
+    for (int _i = 0; _i < 4; _i++) {
+        if (!(_particle_pool_mask & (1 << _i))) {
+            _particle_pool_mask |= (1 << _i);
+            return (opt_ref_Particle){true, &_particle_pool[_i], _i};
+        }
+    }
+    return (opt_ref_Particle){false, NULL, -1};
+}
+
+Result_opt_ref_Particle_TscError make(void) {
+    opt_ref_Particle _pool_0 = Particle_alloc();
+    if (!_pool_0.has_value) {
+        return (Result_opt_ref_Particle_TscError){.ok = false, .error = Error_new(STR_LIT("pool exhausted: Particle"))};
+    }
+    opt_ref_Particle p = _pool_0;
+    p.value->x = 10;
+    return (Result_opt_ref_Particle_TscError){.ok = true, .value = p};
+}
+
+int main(void) {
+    TSC_INIT();
+    return 0;
+}
